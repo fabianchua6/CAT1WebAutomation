f702ce89dd4a9241687f6aa9535564f901a32d64
ljw0913 <jiawei3e1@gmail.com>
2020-10-23 21:13:18 +0800

    added comments to understand the codes

--- scraper.js
+++ scraper.js
@@ -1,12 +1,14 @@
-const puppeteer = require('puppeteer');
-const C = require('./constants');
-const USERNAME_SELECTOR = '#user_login';
+const puppeteer = require('puppeteer'); // main library for web scrapping
+const C = require('./constants'); //contains all the environmental variables
+
+//CSS SELECTOR for username, password textbox and login button on targeted website
+const USERNAME_SELECTOR = '#user_login'; 
 const PASSWORD_SELECTOR = '#pwd';
 const CTA_SELECTOR = '#wp-submit';
-const DOTENV = require('dotenv');
+
 
 async function startBrowser() {
-    const browser = await puppeteer.launch({slowMo:30 }); //slowmo 30ms to ensure credentials are entered in a timely manner
+    const browser = await puppeteer.launch({ slowMo: 30 }); //slowmo 30ms to ensure credentials are entered in a timely manner
     const page = await browser.newPage();
     return { browser, page };
 }
@@ -15,27 +17,34 @@ async function closeBrowser(browser) {
     return browser.close();
 }
 
-async function playTest(url) {
+// core function to scrap CAT 1 details
+async function scrapWeb(url) {
     const { browser, page } = await startBrowser();
     page.setViewport({ width: 1366, height: 1020 });
+
+    // perform series of automation for login
     await page.goto(url);
     await page.click(USERNAME_SELECTOR);
     await page.keyboard.type(C.username);
     await page.click(PASSWORD_SELECTOR);
     await page.keyboard.type(C.password);
     await page.click(CTA_SELECTOR);
+
+    // go to CAT 1 related URL upon logging in successfully
     await page.goto(C.scrap_url);
 
+    // snap a screenshot of the CAT 1 overview
     await page.screenshot({ path: 'weather.png' });
-    await closeBrowser(browser);
 
+    // ends the scrapping session
+    await closeBrowser(browser);
 
 
     //await browser.waitForTarget(()=> false);
 }
 
 (async () => {
-    DOTENV.config();
-    await playTest(C.login_url);
+    // start scraping by logging in
+    await scrapWeb(C.login_url);
     process.exit(1);
 })();
